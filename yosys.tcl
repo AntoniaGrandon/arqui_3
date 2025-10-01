f3dd8c27747dd74616018a7de8f61e6261d9ac76
BobyHuidobro <dhuidobro@miuandes.cl>
2025-09-30 21:30:04 -0300

    entrega 1

--- yosys.tcl
+++ yosys.tcl
@@ -1,5 +1,5 @@
 #EJERCICIO: Rellenar los archivos verilog
-yosys read_verilog computer.v
+yosys read_verilog computer.v alu.v instruction_memory.v mux2.v pc.v register.v testbench.v
 
 yosys synth
 yosys write_verilog out/netlist.v
